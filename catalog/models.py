4bd681332d74b3d6f72e4a8a7bb066102af21f49
aps2201 <halo@aps.web.id>
2017-08-13 18:18:55 +0700

    part 4

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,58 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
 # Create your models here.
-class 
+class Genre(models.Model):
+    name = models.CharField(max_length = 200,help_text = "Enter a Genre")
+    
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author',on_delete = models.SET_NULL,null = True)
+    summary = models.TextField(max_length = 1000,help_text="enter summary")
+    isbn = models.CharField('ISBN',max_length=13,help_text="isbn 13 digit")
+    genre = models.ManyToManyField(Genre,help_text="select genre")
+    def display_genre(self):
+        return ','.join([ genre.name for genre in self.genre.all() [:2] ])
+    display_genre.short_description = 'Genre'
+
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('book-detail', args = [str(self.id)])
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key = True, default=uuid.uuid4, help_text="Unique ID")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL,null = True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null = True, blank = True)
+    
+    LOAN_STATUS = (
+        ('m','Maintenance'),
+        ('o','On Loan'),
+        ('a','Available'),
+        ('r','Reserved'),
+        )
+    
+    status = models.CharField(max_length=1,choices = LOAN_STATUS, blank = True, default = 'd', help_text = "status")
+    
+    class Meta:
+        ordering = ['due_back']
+        
+    def __str__(self):
+        return '%s (%s)' % (self.id,self.book.title)
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField('Born', null = True,blank = True)
+    date_of_death = models.DateField('Died',null=True,blank = True)
+    def get_absolute_url(self):
+        return reverse('author-detail',args=[str(self.id)])
+        
+    def __str__(self):
+        return '%s,%s' % (self.last_name, self.first_name)
