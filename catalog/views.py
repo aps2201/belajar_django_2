a843332306871280b871b8a054a936dc6949675c
Andaru <andaru@detik.com>
2017-08-16 17:15:18 +0700

    di part 5

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre
 
 # Create your views here.
+def index(request):
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    num_instances_available = BookInstance.objects.filter(status__exact = 'a').count()
+    num_authors =  Author.objects.all().count()
