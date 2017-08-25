224be88a8a77d4d9fd9a22dabef0ec5b80fa0363
Andaru <andaru@detik.com>
2017-08-25 16:13:50 +0700

    part 6

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 # Create your views here.
 def index(request):
     num_books = Book.objects.count()
     num_instances = BookInstance.objects.count()
     num_instances_available = BookInstance.objects.filter(status__exact = 'a').count()
-    num_authors =  Author.objects.all().count()
+    num_authors =  Author.objects.count()
+    num_genre = Genre.objects.filter(name__exact = 'Fantasy').count()
+
+    return render(
+        request,
+        'index.html',
+        context = {'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,'num_genre':num_genre}
+        )
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'my_book_list'   # your own name for the list as a template variable
+    queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    template_name = 'books/my_arbitrary_template_name_list.html' 
+                                   
